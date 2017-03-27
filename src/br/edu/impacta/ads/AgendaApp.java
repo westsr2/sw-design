c0c889473e712f18cfbb51b32f3d284222f81259
1410198 <1410198@sala31215.fit.net>
2017-03-27 10:37:19 -0300

    1.0.1

--- src/br/edu/impacta/ads/AgendaApp.java
+++ src/br/edu/impacta/ads/AgendaApp.java
@@ -7,7 +7,10 @@ import static java.lang.System.out;
 
 public class AgendaApp {
 	private static Scanner entrada = new Scanner(System.in);
-	private static List<Contato> contatos = new ArrayList<>();
+	
+	//private static List<Contato> contatos = new ArrayList<>();
+	private static IContatoDao dao = new ContatoDao();
+	
 	public static void main(String[] args) {
 		boolean sair = false;
 		while (!sair) {
@@ -46,11 +49,17 @@ public class AgendaApp {
 		String nome = lerNome();
 		String telefone = lerTelefone();
 		Contato c = new Contato(nome, telefone);
-		if (contatos.contains(c)){
+		
+		//if (contatos.contains(c)){
+		if(dao.existe(c)){	
+			
 			out.println("Este contato ja está cadastrado");
 		}
 		else{
-			contatos.add(c);
+			
+			//contatos.add(c);
+			dao.inserir(c);
+			
 			out.println("Contato inserido!");
 		}
 	}
@@ -84,12 +93,16 @@ public class AgendaApp {
 	private static void buscarContato() {
 		out.println("\nBUSCA DE CONTATOS: ");
 		String nome = lerNome();
-		List<Contato> resultado = new ArrayList<>();
-		for(Contato c: contatos) {
-			if(nome.equals(c.getNome())) {
-				resultado.add(c);
-			}
-		}
+		
+//		List<Contato> resultado = new ArrayList<>();
+//		for(Contato c: contatos) {
+//			if(nome.equals(c.getNome())) {
+//				resultado.add(c);
+//			}
+//		}
+		List<Contato> resultado = dao.buscar(nome);
+		
+		
 		if (resultado.size() == 0) {
 			out.println("Não há contato com este nome!");
 		}else{
